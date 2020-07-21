bc01ca68f3201675b0296c767d45c6d742843405
michaldebicki4 <michaldebicki4@gmail.com>
2020-07-21 19:21:58 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int number =35;
+        int number2 = 12;
+        int number3  = 6;
+
+        int myTotal = number + number2 + number3;
+
+        System.out.println(myTotal);
+
+    }
+}
